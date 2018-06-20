91f5a0d4cb72ce606016d5578e01a0339e53ab7b
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-06-20 10:49:53 +0530

    Misc keycloak grafana integration fixes

--- cloudman/data/update_keycloak_user.sh
+++ cloudman/data/update_keycloak_user.sh
@@ -1,16 +1,18 @@
 #! /bin/sh
 
+username="admin"
+password="{{ .Values.keycloak.keycloak.password }}"
 # get auth token
-token=$(curl -s -d "client_id=admin-cli" -d "username=admin" -d "password=gvl_letmein" -d "grant_type=password" \
-       "http://localhost:8080/auth/realms/master/protocol/openid-connect/token" | jq -r '.access_token')
+token=$(curl -k -s -d "client_id=admin-cli" -d "username=admin" -d "password=$password" -d "grant_type=password" \
+       "https://{{ .Values.global.domain }}/auth/realms/master/protocol/openid-connect/token" | jq -r '.access_token')
 
 # get admin user id
-user_id=$(curl -s -H "Content-Type: application/json" -H "Authorization: bearer $token" http://localhost:8080/auth/admin/realms/master/users/?username=admin | \
+user_id=$(curl -k -s -H "Content-Type: application/json" -H "Authorization: bearer $token" https://{{ .Values.global.domain }}/auth/admin/realms/master/users/?username=admin | \
           jq -r '.[] | select(.username=="admin") | .id')
 
 # update admin user info
-updated_user=$(curl -s -H "Content-Type: application/json" -H "Authorization: bearer $token" http://localhost:8080/auth/admin/realms/master/users/$user_id | \
+updated_user=$(curl -k -s -H "Content-Type: application/json" -H "Authorization: bearer $token" https://{{ .Values.global.domain }}/auth/admin/realms/master/users/$user_id | \
           jq -r '.firstName="{{ .Values.admin_firstname }}" | .lastName="{{ .Values.admin_lastname }}" | .email="{{ .Values.admin_email }}"')
 
 # Save new info
-curl -v -X PUT -H "Content-Type: application/json" -H "Authorization: bearer $token" http://localhost:8080/auth/admin/realms/master/users/$user_id -d "$updated_user"
+curl -k -X PUT -H "Content-Type: application/json" -H "Authorization: bearer $token" https://{{ .Values.global.domain }}/auth/admin/realms/master/users/$user_id -d "$updated_user"
