ff05597d69722877803854460a6c57fc2ec78f77
almahmoud <amahmou4@jhu.edu>
2020-02-29 00:08:35 -0500

    Enable User Registration

--- cloudman/data/update_keycloak.sh
+++ cloudman/data/update_keycloak.sh
@@ -14,12 +14,26 @@ realm_protection=$(curl -k -s -H "Content-Type: application/json" -H "Authorizat
 
 if [ "$realm_protection" = "false" ]
 then
-      
-      # Add Brute Force Detection to Master realm
+
+      # Add Brute Force Detection to 'master' realm
       curl -k -X PUT -H "Content-Type: application/json" -H "Authorization: bearer $token" {{ include "cloudman.root_url" . }}/auth/admin/realms/master -d '{"bruteForceProtected": true, "failureFactor": 5, "maxFailureWaitSeconds": 1800, "minimumQuickLoginWaitSeconds": 300}'
 
 else
       echo "Brute Force Protection is already on."
+fi
+
+# Get current user registration status
+user_registration=$(curl -k -s -H "Content-Type: application/json" -H "Authorization: bearer $token" {{ include "cloudman.root_url" . }}/auth/admin/realms/master | \
+                   jq -r '.registrationAllowed')
+
+if [ "$user_registration" = "false" ]
+then
+
+      # Add User Registration to 'master' realm
+      curl -k -X PUT -H "Content-Type: application/json" -H "Authorization: bearer $token" {{ include "cloudman.root_url" . }}/auth/admin/realms/master -d '{"registrationAllowed": true, "registrationEmailAsUsername": true, "loginWithEmailAllowed": true, "duplicateEmailsAllowed": false}'
+
+else
+      echo "User Registration is already on."
 fi
 
 # Add superuser role
